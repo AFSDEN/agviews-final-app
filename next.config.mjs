43002ff003b38df5c460ca0b9fae2761a1faefe3
AFSDEN <affinitiglobal4u@gmail.com>
2026-08-01 11:14:43 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    // This tells Vercel to completely ignore strict type crashes and build anyway!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // This stops lint errors from blocking your deployment!
     ignoreDuringBuilds: true,
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
+
